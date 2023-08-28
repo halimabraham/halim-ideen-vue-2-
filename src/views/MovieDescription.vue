164d75eeeeec0280242db929b7982d4cf317fff6
Halim Abraham <halimcillo@gmail.com>
2023-08-28 11:09:12 -0600

    added movie description view some style

--- src/views/MovieDescription.vue
+++ src/views/MovieDescription.vue
@@ -1,45 +1,64 @@
 <template>
     <div>
-        <h1 class="subheading mx-3 my-3">Movie Description</h1>
-        <!-- <v-container fill-height fluid>
-            <v-row align="center"
-                justify="center">
-                <v-col>
-                    <v-img
-                    :alt="title"
-                    class="shrink mr-2"
-                    contain 
-                    :src="img"
-                    transition="scale-transition"
-                    width="200">
-                    </v-img>
-                    <p >{{ title }}</p>
-                    <p>{{ release }}</p>
-                    <p>{{ rotten}}</p>
-                    <p>{{ plot }}</p>
+        <v-container class="grey lighten-5">
+            <v-row no-gutters align="center" justify="center">
+                <v-col
+                    cols="12">
+                    <h1 >Movie Description</h1>
                 </v-col>
             </v-row>
-        </v-container> -->
-        <!-- <v-card
-            class="d-flex justify-center mb-6"
-            :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
-            flat
-            tile
-        >
-            <v-img
-                :alt="title"
-                class="shrink mr-2"
-                contain 
-                :src="img"
-                transition="scale-transition"
-                width="200">
-            </v-img>
-            <p >{{ title }}</p>
-            <p>{{ release }}</p>
-            <p>{{ rotten}}</p>
-            <p>{{ plot }}</p>
-        </v-card> -->
-        
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-img
+                        :alt="title"
+                        class="shrink mr-2"
+                        contain 
+                        :src="img"
+                        transition="scale-transition"
+                        height="500"
+                        >
+                    </v-img>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <template>
+                        <v-row no-gutters>
+                            <v-col 
+                                cols="12">
+                                <h3 class="subheading mx-3 my-3">Movie title:</h3>
+                                <p >{{ title }}</p>
+                            </v-col>
+                        </v-row>
+                        <v-row no-gutters>
+                            <v-col 
+                                cols="12">
+                                <h3 class="subheading mx-3 my-3">Release Date:</h3>
+                                <p >{{ release }}</p>
+                            </v-col>
+                        </v-row>
+                        <v-row no-gutters>
+                            <v-col 
+                                cols="12">
+                                <h3 class="subheading mx-3 my-3">Rotten Tomatoes Rating:</h3>
+                                <p >{{ rotten }}</p>
+                            </v-col>
+                        </v-row>
+                        <v-row no-gutters>
+                            <v-col 
+                                cols="12">
+                                <h3 class="subheading mx-3 my-3">Movie plot:</h3>
+                                <p >{{ plot }}</p>
+                            </v-col>
+                        </v-row>
+                    </template>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
